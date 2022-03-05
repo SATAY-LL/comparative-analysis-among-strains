15b460d456c27d1e927a60b65e79e2137668e069
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2022-03-05 11:11:51 +0100

    analyzing differential genes by  normalized scater plots

--- src/python-scripts-from-notebooks/analysis_nrp1_function.py
+++ src/python-scripts-from-notebooks/analysis_nrp1_function.py
@@ -46,7 +46,7 @@ for i in pergene_files:
 
 # -
 
-keys= ['wt_merged','dbem3_merged']
+keys= ['wt_merged','dnrp1_merged']
 
 # ## Volcano plots
 
@@ -82,10 +82,85 @@ volcano_df_nrp1_wt.sort_values(by=['fold_change'], inplace=True)
 volcano_df_nrp1_wt[volcano_df_nrp1_wt["significance"]==True][0:20]
 
 # +
-## Plot number of normalized insertions from WT/ total number of insertions of the library 
-# vs the same for dnrp1
 
 # import excel file with the normalized data
 
-data_norm_pd=pd.read_excel("./postprocessed-data/data_norm_linear_transformation_all_backgrounds.xlsx",
-engine='openpyxl',index_col="Unnamed: 0")
+data_norm_pd=pd.read_excel("../postprocessed-data/data_norm_linear_transformation_per_background.xlsx",
+engine='openpyxl',index_col="background")
+data_norm_pd.drop(columns=["Unnamed: 0","Unnamed: 1"],inplace=True)
+# -
+
+data=data_norm_pd.loc[keys]
+
+# +
+## Plot number of normalized insertions from WT/ total number of insertions of the library 
+# vs the same for dnrp1
+
+variable="reads_over_windows" # "insertions_over_windows"
+var_norm="Reads" #Insertions
+x_1=data.loc["wt_merged"]["reads_over_windows"]/data.loc["wt_merged"]["Reads"].sum()
+y_1=data.loc["dnrp1_merged"]["reads_over_windows"]/data.loc["dnrp1_merged"]["Reads"].sum()
+
+x_0=data.loc["wt_merged"]["insertions_over_windows"]/data.loc["wt_merged"]["Insertions"].sum()
+y_0=data.loc["dnrp1_merged"]["insertions_over_windows"]/data.loc["dnrp1_merged"]["Insertions"].sum()
+
+# x=data.loc["wt_merged"]["Reads"]
+# y=data.loc["dnrp1_merged"]["Reads"]
+
+fig , ax = plt.subplots(ncols=2,figsize=(6,2))
+
+plt.subplots_adjust(wspace=0.5)
+
+#sns.regplot(x,y,fit_reg=True,color="black",marker="o")
+
+ax[0].scatter(x_0,y_0,color="black",marker="o",alpha=0.5)
+
+ax[1].scatter(x_1,y_1,color="black",marker="o",alpha=0.5)
+
+for axes in ax:
+    axes.set_xscale("log")
+    axes.set_yscale("log")    
+
+    axes.set_xlabel("WT")
+    axes.set_ylabel("$\Delta$nrp1")
+
+# -
+
+
+
+test=["wt_merged","bem1-aid_b"]
+data_test=data_norm_pd.loc[test]
+
+# +
+x_1=data_test.loc["wt_merged"]["reads_over_windows"]/data_test.loc["wt_merged"]["Reads"].sum()
+y_1=data_test.loc[test[1]]["reads_over_windows"]/data_test.loc[test[1]]["Reads"].sum()
+
+x_0=data_test.loc["wt_merged"]["insertions_over_windows"]/data_test.loc["wt_merged"]["Insertions"].sum()
+y_0=data_test.loc[[test[1]]]["insertions_over_windows"]/data_test.loc[test[1]]["Insertions"].sum()
+
+# x=data.loc["wt_merged"]["Reads"]
+# y=data.loc["dnrp1_merged"]["Reads"]
+
+fig , ax = plt.subplots(ncols=2,figsize=(6,2))
+
+plt.subplots_adjust(wspace=0.5)
+
+# sns.regplot(x_0,y_0,fit_reg=True,color="black",marker="o",ax=ax[0])
+# sns.regplot(x_1,y_1,fit_reg=True,color="black",marker="o",ax=ax[1])
+
+ax[0].scatter(x_0,y_0,color="black",marker="o",alpha=0.5)
+
+ax[1].scatter(x_1,y_1,color="black",marker="o",alpha=0.5)
+
+
+for axes in ax:
+    # axes.set_xscale("log")
+    # axes.set_yscale("log")    
+    axes.set_xlim(0,0.01)
+    axes.set_ylim(0,0.01)
+    axes.set_xlabel("WT")
+    axes.set_ylabel("$\Delta$bem1")
+
+# -
+
+
