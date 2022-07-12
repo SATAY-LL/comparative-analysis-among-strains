d025ea5248cb6b7d7599660809915b3bd760fab1
Leila Inigo de la Cruz <leilaicruz@gmail.com>
2022-07-12 15:32:29 +0200

    streamlit

--- src/TO_DO.md
+++ src/TO_DO.md
@@ -4,3 +4,5 @@ that generates every time the dataframe to use for extracting the reads per doma
     - I found it is better to generate the data as I use it , instead of loading oot from excel and to all annoying transformations to have the data as I want to be use for further functions. 
 
 - [ ] Refactor notebook analysis_proteins_domains.ipynb , so that I have a function that generates the genomic locations , insertions per domain , etc.  
+
+- [ ] Add test functions for the main functionalities
--- src/streamlit_example.py
+++ src/streamlit_example.py
@@ -1,19 +1,31 @@
+from email.policy import default
+from re import template
+from matplotlib.pyplot import title
 import pandas as pd
 import numpy as np
 import plotly.express as px
 from sqlalchemy import column
+from from_excel_to_list import from_excel_to_list
 import streamlit as st
 
 st.set_page_config(page_title="Example satay data",
 page_icon=":partying_face:",layout="wide")
 
-df=pd.read_excel(
-    io="postprocessed-data/wt_a_pergene_insertions.xlsx",
-    engine='openpyxl',
-    index_col="Unnamed: 0")
- 
-df.rename(columns={"Gene name":"Gene_name"},inplace=True)
+st.markdown("##")
+st.title(":eight_spoked_asterisk: Satay example data ")
 
+@st.cache
+def get_data_from_excel():
+
+    df=pd.read_excel(
+        io="../postprocessed-data/wt_a_pergene_insertions.xlsx",
+        engine='openpyxl',
+        index_col="Unnamed: 0")
+ 
+    df.rename(columns={"Gene name":"Gene_name"},inplace=True)
+    return df
+
+df=get_data_from_excel()
 #-----sidebar-----
 
 st.sidebar.header("Please Filter here:")
@@ -21,6 +33,7 @@ st.sidebar.header("Please Filter here:")
 genes=st.sidebar.multiselect(
     "Select the gene of interest",
     options=df["Gene_name"].unique(),
+    default=df["Gene_name"][0:10]
     )
 
 reads=st.sidebar.multiselect(
@@ -45,3 +58,56 @@ df_selection=df.query(
 st.dataframe(df_selection)
 
 # ----MainPage----
+
+st.title(":game_die: Library numbers")
+
+st.markdown("##")
+
+## Some viz
+
+total_insertions=df["Insertions"].sum()
+
+total_reads=df["Reads"].sum()
+
+
+left_column,middle_column=st.columns(2)
+
+with left_column:
+    st.subheader("Total insertions:")
+    st.subheader(f"{total_insertions}")
+with middle_column:
+    st.subheader("Total reads:")
+    st.subheader(f'{total_reads}')
+
+
+st.markdown("---")
+
+st.title(" :bar_chart: Insertions and reads distributions")
+st.markdown("##")
+
+fig_histogram_insertions=px.histogram(
+    df["Insertions"],title="Insertions histogram",
+    template="plotly_white"
+)
+fig_histogram_reads=px.histogram(
+    df["Reads"],title="Reads histograms",
+    template="plotly_white"
+)
+
+fig_histogram_insertions.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
+
+fig_histogram_reads.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
+
+left_column,right_column=st.columns(2)
+left_column.plotly_chart(fig_histogram_insertions,use_container_width=True)
+right_column.plotly_chart(fig_histogram_reads,use_container_width=True)
+
+
+
+
